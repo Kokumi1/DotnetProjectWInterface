913ece0dfb48ad40576bb0ff45e7a052e6d63db5
Naplucia <72755048+Kokumi1@users.noreply.github.com>
2025-01-20 10:59:40 +0100

    Add Front end with interface

---
+++ WPFFrontEnd/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFFrontEnd.UI;
+
+namespace WPFFrontEnd
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(sender is Button button)
+            {
+                switch (button.Tag.ToString())
+                {
+                    case "Opt1":
+                        DynamicContent.Content = new ListOptionControl();
+                        break;
+
+                    case "Opt2":
+                        DynamicContent.Content = new AddOptionControl();
+                        break;
+
+                    case "Opt3":
+                        DynamicContent.Content = new TextBlock
+                        {
+                            Text = "Contenu3",
+                            FontSize = 24
+                        };
+                        break;
+
+                    default:
+                        DynamicContent.Content = null;
+                        break;
+                }
+            }
+        }
+    }
+}
